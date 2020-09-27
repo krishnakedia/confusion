4cb9753edd3054b4bc7236648a32328a924b07a7
krishna kedia <kediakrishna65@gmail.com>
2020-09-27 19:56:29 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { navbar, NavbarBrand } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/Menu';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <navbar dark colour="danger">
+      <Navbar dark colour="primary">
         <div className="container">
           <NavbarBrand href='/'>
             Coursera react part 2
           </NavbarBrand>
         </div>
-      </navbar>
+      </Navbar>
+      <Menu />
       </div>
   );
 }
