7baa810a84e45f4466bc77798efd122ea56e6b97
krishna kedia <kediakrishna65@gmail.com>
2020-09-27 19:18:38 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <navbar dark colour="danger">
+        <div className="container">
+          <NavbarBrand href='/'>
+            Coursera react part 2
+          </NavbarBrand>
+        </div>
+      </navbar>
+      </div>
   );
 }
 
